6d2d289c9673e5612da594ddc96f25070c8da86e
Steven Wallace <snwallace@gmail.com>
2012-11-18 11:16:21 -0600

    Bit of cleanup and error checking.

--- apps.py
+++ apps.py
@@ -3,8 +3,11 @@ def command(function):
     function.is_command = True
 
 class App(object):
-    commands = {}
-
+    def send_error(self, command, message, status = 'error'):
+        error = copy(command)
+        error['status'] = status
+        error['message'] = message
+        self.connection.send_json(error)
     def __init__(self, connection):
         self.connection = connection
 
@@ -12,19 +15,29 @@ class App(object):
         pass
 
     def run(self, command):
-        c = command['command']
-        args = command['args']
+        try:
+            c = command['command']
+            args = command['args']
+        except KeyError:
+            self.send_error(command, 'was missing values')
+            return
         f = getattr(self, c, None)
         if not f or not getattr(f, 'is_command', False):
-            error = copy(command)
-            error['status'] = 'error'
-            error['message'] = 'command not found'
-            self.connection.send_json(error)
+            self.send_error(command, 'command not found')
             return
 
         return self.commands[c](self, **args)
 
 class LoginApp(App):
+    """
+    This is the starting app for connections.
+    This facilitates any authentication we want to do
+    and let's them specify what kind of connection they want.
+
+    This should detect all the game plugins and expose them.
+    Also, the arena app, and any others we think of later.
+    """
+
     @command
     def login(self, username, password, type):
         pass
