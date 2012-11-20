1f3803358b2dafc671ad217800c3a024ad503cc4
Goldman <brianwgoldman@acm.org>
2012-11-20 12:30:56 -0500

    Outline of GameApp

--- apps.py
+++ apps.py
@@ -1,13 +1,18 @@
 from copy import copy
+from collections import defaultdict
+
+
 def command(function):
     function.is_command = True
 
+
 class App(object):
-    def send_error(self, command, message, status = 'error'):
+    def send_error(self, command, message, status='error'):
         error = copy(command)
         error['status'] = status
         error['message'] = message
         self.connection.send_json(error)
+
     def __init__(self, connection):
         self.connection = connection
 
@@ -28,6 +33,7 @@ class App(object):
 
         return self.commands[c](self, **args)
 
+
 class LoginApp(App):
     """
     This is the starting app for connections.
@@ -41,3 +47,39 @@ class LoginApp(App):
     @command
     def login(self, username, password, type):
         pass
+
+
+class GameApp(App):
+    games = defaultdict(dict)
+
+    @command
+    def join_game(self, game_type, **game_details):
+        # Check if game you want to join exists, otherwise create it.
+        # If game exists, make certain you can join it
+        # Transition to the GameApp state
+        # TODO Sanitize game_type
+        if 'game_number' in game_details:
+            game_number = game_details['game_number']
+        else:
+            # If you don't specify a game number, find the lowest unused
+            game_number = 1
+            while game_number in self.games[game_type]:
+                game_number += 1
+        if game_number in self.games[game_type]:
+            # Join existing game
+            game = self.games[game_type][game_number]
+        else:
+            # Create new game
+            try:
+                game_mod = __import__('plugins.%s.game' % game_type,
+                                      fromlist=['*'])
+            except ImportError:
+                #TODO Send some error
+                return
+            game = game_mod.Game()
+            self.games[game_type][game_number] = game
+        game.add_connection(self, game_details)
+
+        #TODO Make certain game objects are removed from self.games when they
+        #TODO end.  Also, its more minor but game_type's should be removed as
+        #TODO well.
