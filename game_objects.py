8c3a0164b97fa2988a38d2f126117ba86581b25e
Steven Wallace <snwallace@gmail.com>
2013-06-01 17:23:39 -0500

    Metaclass trickery!
    Trying to cleanly keep track of all the game objects.

--- game_objects.py
+++ game_objects.py
@@ -1,46 +1,21 @@
-import json
 from collections import defaultdict
 
-
-class Game(object):
-    # Shell game to show interaction
-    def __init__(self):
-        self.highest_id = 0
-        self.additions = []
-        self.changes = defaultdict(dict)
-        self.deletions = []
-
-    def next_id(self):
-        self.highest_id += 1
-        return self.highest_id
-
-    def flush(self):
-        output = []
-        for added in self.additions:
-            output.append({'action': 'add', 'id': added.id, 'type':added.__class__.__name__})
-        for id, changes in self.changes.items():
-            output.append({'action': 'update', 'id': id, 'changes': changes})
-        for deleted in self.deletions:
-            output.append({'action': 'remove', 'id': deleted.id})
-
-        self.additions = []
-        self.changes = defaultdict(dict)
-        self.deletions = []
-        return output
-
+class GameObjectMeta(type):
+    def __new__(meta, name, bases, dct):
+        cls = type.__new__(meta, name, bases, dct)
+        cls._game.object_types[name] = cls
+        return cls
 
 class GameObject(object):
     # Root game object
     game_state_attributes = set()
 
-    def __init__(self, game):
-        self.game = game
-        self.id = game.next_id()
+    def __init__(self):
+        self.id = self.game.next_id()
         self.game.additions.append(self)
-        # TODO Register self with game objects stuff, which also handles animation
 
     def __del__(self):
-        # TODO Unregister self with game objects stuff, which also handles animation
+        self.game.remove(self)
         pass
 
     def __setattr__(self, name, value):
@@ -54,41 +29,24 @@ class GameObject(object):
         attributes['id'] = self.id
         return attributes
 
-if __name__ == '__main__':
-    # TODO: Separate tests out into their own part
-    class XUnit(GameObject):
-        # Example Unit that tracks the x attribute
-        game_state_attributes = set(['x']) | GameObject.game_state_attributes
+class GameMeta(type):
+    def __new__(meta, name, bases, dct):
+        cls = type.__new__(meta, name, bases, dct)
+        class Object(GameObject):
+            _game = cls
+            __metaclass__ = GameObjectMeta
+        cls.Object = Object
+        return cls
 
-        def __init__(self, game, x):
-            super(XUnit, self).__init__(game)
-            self.x = x
-            self.y = 'y'
+class Game(object):
+    object_types = {}
+    __metaclass__ = GameMeta
+    # Shell game to show interaction
+    def __init__(self):
+        self.highest_id = 0
+        self.additions = []
+        self.changes = defaultdict(dict)
+        self.deletions = []
 
-
-    class YUnit(GameObject):
-        # Example Unit that tracks the y attribute
-        game_state_attributes = set(['y']) | GameObject.game_state_attributes
-
-        def __init__(self, game, x):
-            super(YUnit, self).__init__(game)
-            self.x = x
-            self.y = 'y'
-
-    game = Game()
-    g = GameObject(game)
-    print g.game
-    test = XUnit(game, 14)
-    print "INITED", game.flush()
-    test.x = 0
-    test.y = 7
-    print 'Writted', game.flush()
-
-    other = YUnit(game, 14)
-    print "INITED", game.flush()
-    other.x = 0
-    other.y = 7
-    print 'Writted', game.flush()
-
-    print test.jsonize()
-    print other.jsonize()
+    def add_object(self, object):
+        self.additions.append(object)
