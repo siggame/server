2255a98acad7302f943b2cc7830f532d1fac3780
Steven Wallace <snwallace@gmail.com>
2013-06-01 15:28:19 -0500

    Made json protocol a bit pickier about messages.

--- json_protocol.py
+++ json_protocol.py
@@ -18,13 +18,21 @@ class JSONProtocol(Int32StringReceiver):
         try:
             expr = json.loads(string)
         except:
-            print 'Bad message received: %s' % string
-            error = {'type': 'parse error'}
-            error['args'] = {'message': string}
-            self.send_json(error)
+            self.send_message('parse error', message=string, reason='failed to parse as json')
             return
-        self.app.run(expr)
+        if not isinstance(expr, dict):
+            self.send_message('parse error', message=string, reason='commands must be objects')
+        elif 'type' not in expr or not isinstance(expr['type'], basestring):
+            self.send_message('parse error', message=string, reason='commands must contain type string')
+        elif 'args' not in expr or not isinstance(expr['args'], dict):
+            self.send_message('parse error', message=string, reason='commands must contain args object')
+        else:
+            self.app.run(expr)
 
     def send_json(self, object):
         message = json.dumps(object)
         self.sendString(message)
+
+    def send_message(self, type, **kwargs):
+        message = {'type': type, 'args': kwargs}
+        self.send_json(message)
