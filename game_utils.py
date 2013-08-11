da7c150e323b762e1815a17bec20580cba0afe3c
Steven Wallace <snwallace@gmail.com>
2013-08-11 14:21:41 -0500

    Would this be cleaner?

--- game_utils.py
+++ game_utils.py
@@ -25,45 +25,38 @@ class Grid(object):
 
 class PathFinder():
 
-    def f_score(self, h, g):
-        return self.h_score(h[0], h[1]) + self.g_score(g[0], g[1])
-
-    def g_score(self, source, current):
-        return self.distance(source[0], source[1], current[0], current[1])
-
     def h_score(self, current, dest):
         return self.distance(current[0], current[1], dest[0], dest[1])
 
-    def getAdjacent(self, source):
-        pass
+    def get_adjacent(self, source):
+        return []
 
-    def pathFind(self,  sourceX, sourceY, destX, destY):
-        closedSet = set()
-        closedTup = set()
-         # 0 = h, 1 = current (x,y), 2 = parent (x,y) 3 = g
-        open = [(self.h_score((sourceX, sourceY), (destX, destY)), (sourceX, sourceY), None, 0)]
-        openTup = [(sourceX, sourceY)]
-        path = []
+    def find_path(self,  source, dest):
+        closed = {source: None}
+        if source == dest:
+            return [source]
+         # 0 = f, 1 = current (x,y), 2 = g
+        open = [ (self.h_score(source, dest), source, 0) ]
         while len(open) > 0:
-            current = heappop(open)
-            if current[1] == (destX, destY):
-                return self.makePath()
-            closedSet.add(current)
-            closedTup.add(current[1])
-            openTup.remove(current[1])
-            open.remove(current)
-            for neighbor in self.getAdjacent((current[1][0], current[1][1])):
-                if neighbor in closedSet:
-                  continue
-                g = current[3] + self.distance(current[1][0], current[1][1], neighbor[0], neighbor[1])
-                if neighbor == (destX, destY) or self.distance(neighbor[0], neighbor[1], sourceX, sourceY) <= g+1 and neighbor not in openTup:
-                  pass
+            h, point, g = heappop(open)
+            for neighbor in self.get_adjacent(point):
+                if neighbor in closed:
+                    continue
+                closed[neighbor] = point
+                if neighbor == dest:
+                    return self.make_path(closed, dest)
+                heappush(open, (g+1+self.hscore(neighbor, dest),
+                    neighbor, g+1) )
+        return None
 
-    def goTo(self, sourceX, sourceY, destX, destY):
-        pass
+    def make_path(self, closed, dest):
+        current = dest
+        path = [current]
+        while closed[current]:
+            current = closed[current]
+            path.append(current)
 
-    def makePath(self):
-        pass
+        return reversed(path)
 
     def distance(self, x1, y1, x2, y2):
         return abs(x2-x1) + abs(y2-y1)
