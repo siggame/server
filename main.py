4132ef214d7a6d2581f19be96613e592f26ad27f
Steven Wallace <snwallace@gmail.com>
2013-08-20 21:37:03 -0500

    Speaking of logging!

--- main.py
+++ main.py
@@ -3,12 +3,16 @@
 from twisted.internet import epollreactor
 epollreactor.install()
 import timer
+import logging
 
 from twisted.internet import protocol, reactor
 from json_protocol import JSONProtocol
 
 if __name__ == '__main__':
     port = 19000
+    #Turn the logging all the way up
+    #TODO: Make a flag for this
+    logging.basicConfig(level=logging.DEBUG)
     f = protocol.ServerFactory()
     f.protocol = JSONProtocol
     reactor.listenTCP(port, f)
